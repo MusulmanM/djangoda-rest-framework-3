a4ba609561e13dabbff976a25c6193751cdec969
MusulmanM <mmmmmmvvv200@gmail.com>
2026-02-06 15:07:33 +0500

    YOUR TEXT

---
+++ uchinchi/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from uchinchi.views import (Salom1vset, Salom2vset, Salom3vset, Salom4vset, Salom5vset)
+
+
+
+router = DefaultRouter()
+router.register(r"1", Salom1vset, basename="salom1")
+router.register(r"2", Salom2vset, basename="salom2")
+router.register(r"3", Salom3vset, basename="salom3")
+router.register(r"4", Salom4vset, basename="salom4")
+router.register(r"5", Salom5vset, basename="salom5")
+
+
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
